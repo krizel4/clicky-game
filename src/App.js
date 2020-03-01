fecc26a5570c3e7fc3d8740d467d33dacfec67a3
Krizel Minnema <krizelminnema@gmail.com>
2020-03-01 14:14:49 -0800

    adding images for the game

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// =============
+// Following list are things to do and not necessarily things that will appear in the app.js
+// =============
+// Database for images with click boolean - true and false
+// function to shuffle cards after a click
+// Increment score for every false to true image clicked
+// If an image that's set to true is clicked again, trigger game loss.
+// Once game is set to lost, announce final score
+// Give option to start game over again
+// If they restart the game, the score clears out and cards reshuffle.
+
+
+
 function App() {
   return (
     <div className="App">
